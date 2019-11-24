21233be99aeb4458bc2b7c7f36298f9fcb3d00f1
Nihagy <valdenor.michael@gmail.com>
2019-11-24 03:39:17 -0300

    limitar string

--- Projeto_bibliotecaC/testes.c
+++ Projeto_bibliotecaC/testes.c
@@ -147,19 +147,61 @@ int main(){
 // }
 
 
-int main(){
+// int main(){
 
-    FILE *f;
+//     FILE *f;
     
-    // leitura de arquivo texto
-    f = fopen("arquivo.txt", "r");
-    // escrita de arquivo texto
-    f = fopen("arquivo.txt", "w");
-    // leitura de arquivo binario
-    f = fopen("arquivo.txt","rb";
-    // escrita de arquivos binario
-    f = fopen("arquivo.txt", "wb");
+//     // leitura de arquivo texto
+//     f = fopen("arquivo.txt", "r");
+//     // escrita de arquivo texto
+//     f = fopen("arquivo.txt", "w");
+//     // leitura de arquivo binario
+//     f = fopen("arquivo.txt","rb";
+//     // escrita de arquivos binario
+//     f = fopen("arquivo.txt", "wb");
 
-    system("pause")
+//     system("pause")
+//     return 0;
+// }
+
+//============================================================================
+
+// limitar string
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>//para utilizar o getch()
+#include <ctype.h>//para utilizar o isdigit()
+ 
+int main(void)
+{
+    char digitado[50];//armazena o que foi digitado pelo usuário
+    char c;//armazena cada caractere digitado pelo usuário
+    int i=0;//variável para controlar o índice do vetor de caracteres
+   
+    printf ("Informe seu nome: ");
+   
+        do
+        {
+       c=getch();//captura o caractere digitado pelo usuário
+           if (isalpha(c)!=0 || c==32)//se for uma letra
+           {
+          digitado[i] = c;//armazena no vetor de caracteres
+          i++;//incrementa o índice do vetor de caracteres
+          printf ("%c", c);//exibe o caractere digitado
+       }
+       else if(c==8&&i)//se for pressionada a tecla BACKSPACE e houver caracteres já digitados
+       {
+          digitado[i]='\0';//o índice atual do vetor de caracteres recebe a terminação da string
+          i--;//o índice do vetor de caracteres é decrementado
+          printf("\b \b");//o caractere digitado é apagado da tela
+       }
+    }while(c!=13);//o loop vai ser executado até que a tecla pressionada seja o ENTER (código 13 na tabela ASCII)
+   
+    digitado[i]='\0';//o índice atual do vetor de caracteres recebe a terminação da string
+   
+    printf ("\n\nSeja bem-vindo, %s\n\n", digitado);
+   
+        system ("pause");
     return 0;
 }
